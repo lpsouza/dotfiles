edc74ce22c2f1a0f198fab26ee4a1c336f3125aa
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2024-10-30 12:24:35 -0300

    Remove Neovim README and update Neo-tree plugin settings

--- nvim/.config/nvim/README.md
+++
@@ -1,7 +0,0 @@
-# Neovim configuration
-
-This is my personal Neovim configuration. It is a work in progress and I am constantly tweaking it.
-
-## Installation
-
-Copy the contents of this folder to `~/.config/nvim/`.
--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -7,6 +7,21 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
+    require('neo-tree').setup {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = false,
+	      hide_dotfiles = false,
+	      hide_gitignored = true,
+	      hide_by_name = {
+        },
+	      never_show = {
+          '.git',
+        },
+      },
+    },
+  }
 		vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", {})
 		vim.keymap.set("n", "<leader>t", ":Neotree buffers reveal float<CR>", {})
 	end,
